b6b1cbde85f15e3525b36e4c68110003ad2e975a
Kay Grewe <Kay.Grewe@protonmail.com>
2023-10-21 14:44:23 +0200

    chore: forget value 5 as a test value...

--- utilitys.s
+++ utilitys.s
@@ -11,58 +11,63 @@
 ;;      could be done better as the aoc problems will be taxing enough without waiting for hours
 ;;      for the string conversion !
 AsciiToInt:
-  cmp.b           #$30,d6           ; Check value if it's below our threshold >= 0x30 '0'
-  blt             .failure
-  cmp.b           #$39,d6           ; Check value if it's below our threshold <= 0x39 '9'
-  bgt             .failure
+  cmp.b            #$30,d6             ; Check value if it's below our threshold >= 0x30 '0'
+  blt              .failure
+  cmp.b            #$39,d6             ; Check value if it's below our threshold <= 0x39 '9'
+  bgt              .failure
   .conversion:
-  move.b          d6,d7
-  subi.b          #$30,d7           ; Subtract the lowest Symbol of the Ascii Digit Table (0x30 '0') from our input
-  rts                               ; Done the result should be inside our d7 register
+  move.b           d6,d7
+  subi.b           #$30,d7             ; Subtract the lowest Symbol of the Ascii Digit Table (0x30 '0') from our input
+  rts                                  ; Done the result should be inside our d7 register
   .failure:
-  move.l          #$FFFFFFFF,d7
+  move.l           #$FFFFFFFF,d7
   rts
 
 
 ascii_to_int_tests:
-  lea             zero,a0
-  lea             result_zero,a1
+  lea              zero,a0
+  lea              result_zero,a1
+  lea              end_of_checks,a3
 .test_loop:
-  move.b          (a0)+,d6
-  move.b          (a1)+,d5
-  jsr             AsciiToInt
-  cmp.b           d5,d7
-  bne             .failure
-  jmp             .test_loop        ;This is fine for now but later on i should check if we reached the end of the testing range and just get the fuck out
+  move.b           (a0)+,d6
+  move.b           (a1)+,d5
+  jsr              AsciiToInt
+  cmp.b            d5,d7
+  bne              .failure
+  cmp.l            a1,a3
+  beq              .win
+  jmp              .test_loop          ;This is fine for now but later on i should check if we reached the end of the testing range and just get the fuck out
 
 .failure:
   nop
-  jmp             .failure          ;Stuck here but there is a bug that needs fixing so that's good :)
+  jmp              .failure            ;Stuck here but there is a bug that needs fixing so that's good :)
   
 .win:
-  nop
-  jmp             .win              ;Huge win everything was as expected AWESOME :D
+  rts                                  ;Huge win everything was as expected AWESOME :D 
 
 
-  zero            : dc.b    '0'
-  one             : dc.b    '1'
-  two             : dc.b    '2'
-  three           : dc.b    '3'
-  four            : dc.b    '4'
-  five            : dc.b    '6'
-  seven           : dc.b    '7'
-  eight           : dc.b    '8'
-  nine            : dc.b    '9'
+  zero             : dc.b '0'
+  one              : dc.b '1'
+  two              : dc.b '2'
+  three            : dc.b '3'
+  four             : dc.b '4'
+  five             : dc.b '5'
+  six              : dc.b '6'
+  seven            : dc.b '7'
+  eight            : dc.b '8'
+  nine             : dc.b '9'
 
-  inv_a           : dc.b    'a'
+  inv_a            : dc.b 'a'
 
-  result_zero     : dc.b 0
-  result_one      : dc.b 1
-  result_two      : dc.b 2
-  result_three    : dc.b 3
-  result_four     : dc.b 4
-  result_five     : dc.b 5
-  result_six      : dc.b 6
-  result_seven    : dc.b 7
-  result_eight    : dc.b 8
-  result_nine     : dc.b 9
+  result_zero      : dc.b 0
+  result_one       : dc.b 1
+  result_two       : dc.b 2
+  result_three     : dc.b 3
+  result_four      : dc.b 4
+  result_five      : dc.b 5
+  result_six       : dc.b 6
+  result_seven     : dc.b 7
+  result_eight     : dc.b 8
+  result_nine      : dc.b 9
+  result_inv       : dc.b $FFFFFFFF
+  end_of_checks    : dc.b $FF
